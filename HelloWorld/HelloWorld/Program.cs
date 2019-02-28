f21a392ae2380856bb6572fec1ad8bd082acce36
Alexandra Plassaras <v-alplas@microsoft.com>
2019-02-28 11:05:27 -0800

    added txt file and changed program.cs

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -8,6 +8,9 @@ namespace HelloWorld
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine();
+            
+            Console.WriteLine("Let's do this!");
+            Console.ReadLine)();
 
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
